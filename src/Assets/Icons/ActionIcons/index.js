73bd7af5f0b4a9c724ae5b182ed7b110f907db44
Nguyen Duc Thang <aloducthang@gmail.com>
2024-01-24 23:07:34 +0700

    Setup Icon

---
+++ src/Assets/Icons/ActionIcons/index.js
@@ -0,0 +1,273 @@
+export function SortIcon(props) {
+    return (
+        <svg
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            height="24"
+            width="24"
+            {...props}
+        >
+            <path d="M18.5 16.44V6.75a.75.75 0 00-1.5 0v9.69l-2.22-2.22a.75.75 0 10-1.06 1.06l3.5 3.5a.75.75 0 001.06 0l3.5-3.5a.75.75 0 10-1.06-1.06l-2.22 2.22zM2 7.25a.75.75 0 01.75-.75h9.5a.75.75 0 010 1.5h-9.5A.75.75 0 012 7.25zm0 5a.75.75 0 01.75-.75h5.5a.75.75 0 010 1.5h-5.5a.75.75 0 01-.75-.75zm0 5a.75.75 0 01.75-.75h3.5a.75.75 0 010 1.5h-3.5a.75.75 0 01-.75-.75z" />
+        </svg>
+    );
+}
+
+export function AddIcon(props) {
+    return (
+        <svg
+            viewBox="0 0 512 512"
+            fill="currentColor"
+            height="24"
+            width="24"
+            {...props}
+        >
+            <path
+                fill="none"
+                stroke="currentColor"
+                strokeMiterlimit={10}
+                strokeWidth={32}
+                d="M448 256c0-106-86-192-192-192S64 150 64 256s86 192 192 192 192-86 192-192z"
+            />
+            <path
+                fill="none"
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={32}
+                d="M256 176v160M336 256H176"
+            />
+        </svg>
+    );
+}
+
+export function RemoveIcon(props) {
+    return (
+        <svg
+            viewBox="0 0 512 512"
+            fill="currentColor"
+            height="24"
+            width="24"
+            {...props}
+        >
+            <path
+                fill="none"
+                stroke="currentColor"
+                strokeMiterlimit={10}
+                strokeWidth={32}
+                d="M448 256c0-106-86-192-192-192S64 150 64 256s86 192 192 192 192-86 192-192z"
+            />
+            <path
+                fill="none"
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={32}
+                d="M336 256H176"
+            />
+        </svg>
+    );
+}
+
+export function PersonAddIcon(props) {
+    return (
+        <svg
+            viewBox="0 0 512 512"
+            fill="currentColor"
+            height="24"
+            width="24"
+            {...props}
+        >
+            <path
+                fill="none"
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={32}
+                d="M376 144c-3.92 52.87-44 96-88 96s-84.15-43.12-88-96c-4-55 35-96 88-96s92 42 88 96z"
+            />
+            <path
+                fill="none"
+                stroke="currentColor"
+                strokeMiterlimit={10}
+                strokeWidth={32}
+                d="M288 304c-87 0-175.3 48-191.64 138.6-2 10.92 4.21 21.4 15.65 21.4H464c11.44 0 17.62-10.48 15.65-21.4C463.3 352 375 304 288 304z"
+            />
+            <path
+                fill="none"
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={32}
+                d="M88 176v112M144 232H32"
+            />
+        </svg>
+    );
+}
+
+export function FilterListIcon(props) {
+    return (
+        <svg
+            fill="currentColor"
+            viewBox="0 0 16 16"
+            height="24"
+            width="24"
+            {...props}
+        >
+            <path d="M6 10.5a.5.5 0 01.5-.5h3a.5.5 0 010 1h-3a.5.5 0 01-.5-.5zm-2-3a.5.5 0 01.5-.5h7a.5.5 0 010 1h-7a.5.5 0 01-.5-.5zm-2-3a.5.5 0 01.5-.5h11a.5.5 0 010 1h-11a.5.5 0 01-.5-.5z" />
+        </svg>
+    );
+}
+
+export function DropDownCircleIcon(props) {
+    return (
+        <svg
+            viewBox="0 0 512 512"
+            fill="currentColor"
+            height="24"
+            width="24"
+            {...props}
+        >
+            <path d="M342.43 238.23l-74.13 89.09a16 16 0 01-24.6 0l-74.13-89.09A16 16 0 01181.86 212h148.28a16 16 0 0112.29 26.23z" />
+            <path
+                fill="none"
+                stroke="currentColor"
+                strokeMiterlimit={10}
+                strokeWidth={32}
+                d="M448 256c0-106-86-192-192-192S64 150 64 256s86 192 192 192 192-86 192-192z"
+            />
+        </svg>
+    );
+}
+
+export function CancleIcon(props) {
+    return (
+        <svg
+            viewBox="0 0 512 512"
+            fill="currentColor"
+            height="24"
+            width="24"
+            {...props}
+        >
+            <path
+                fill="none"
+                stroke="currentColor"
+                strokeMiterlimit={10}
+                strokeWidth={32}
+                d="M448 256c0-106-86-192-192-192S64 150 64 256s86 192 192 192 192-86 192-192z"
+            />
+            <path
+                fill="none"
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={32}
+                d="M320 320L192 192M192 320l128-128"
+            />
+        </svg>
+    );
+}
+
+export function MoreIcon(props) {
+    return (
+        <svg
+            fill="none"
+            stroke="currentColor"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            viewBox="0 0 24 24"
+            height="24"
+            width="24"
+            {...props}
+        >
+            <path d="M13 12 A1 1 0 0 1 12 13 A1 1 0 0 1 11 12 A1 1 0 0 1 13 12 z" />
+            <path d="M20 12 A1 1 0 0 1 19 13 A1 1 0 0 1 18 12 A1 1 0 0 1 20 12 z" />
+            <path d="M6 12 A1 1 0 0 1 5 13 A1 1 0 0 1 4 12 A1 1 0 0 1 6 12 z" />
+        </svg>
+    );
+}
+
+export function DragIcon(props) {
+    return (
+        <svg fill="none" viewBox="0 0 15 15" height="24" width="24" {...props}>
+            <path
+                fill="currentColor"
+                fillRule="evenodd"
+                d="M5.5 4.625a1.125 1.125 0 100-2.25 1.125 1.125 0 000 2.25zm4 0a1.125 1.125 0 100-2.25 1.125 1.125 0 000 2.25zM10.625 7.5a1.125 1.125 0 11-2.25 0 1.125 1.125 0 012.25 0zM5.5 8.625a1.125 1.125 0 100-2.25 1.125 1.125 0 000 2.25zm5.125 2.875a1.125 1.125 0 11-2.25 0 1.125 1.125 0 012.25 0zM5.5 12.625a1.125 1.125 0 100-2.25 1.125 1.125 0 000 2.25z"
+                clipRule="evenodd"
+            />
+        </svg>
+    );
+}
+
+export function VisibilityIcon(props) {
+    return (
+        <svg
+            viewBox="0 0 512 512"
+            fill="currentColor"
+            height="24"
+            width="24"
+            {...props}
+        >
+            <path
+                fill="none"
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={32}
+                d="M255.66 112c-77.94 0-157.89 45.11-220.83 135.33a16 16 0 00-.27 17.77C82.92 340.8 161.8 400 255.66 400c92.84 0 173.34-59.38 221.79-135.25a16.14 16.14 0 000-17.47C428.89 172.28 347.8 112 255.66 112z"
+            />
+            <path
+                fill="none"
+                stroke="currentColor"
+                strokeMiterlimit={10}
+                strokeWidth={32}
+                d="M336 256 A80 80 0 0 1 256 336 A80 80 0 0 1 176 256 A80 80 0 0 1 336 256 z"
+            />
+        </svg>
+    );
+}
+
+export function VisibilityOffIcon(props) {
+    return (
+        <svg
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            height="24"
+            width="24"
+            {...props}
+        >
+            <path d="M2 5.27L3.28 4 20 20.72 18.73 22l-3.08-3.08c-1.15.38-2.37.58-3.65.58-5 0-9.27-3.11-11-7.5.69-1.76 1.79-3.31 3.19-4.54L2 5.27M12 9a3 3 0 013 3 3 3 0 01-.17 1L11 9.17A3 3 0 0112 9m0-4.5c5 0 9.27 3.11 11 7.5a11.79 11.79 0 01-4 5.19l-1.42-1.43A9.862 9.862 0 0020.82 12 9.821 9.821 0 0012 6.5c-1.09 0-2.16.18-3.16.5L7.3 5.47c1.44-.62 3.03-.97 4.7-.97M3.18 12A9.821 9.821 0 0012 17.5c.69 0 1.37-.07 2-.21L11.72 15A3.064 3.064 0 019 12.28L5.6 8.87c-.99.85-1.82 1.91-2.42 3.13z" />
+        </svg>
+    );
+}
+
+export function ToggleLeftIcon(props) {
+    return (
+        <svg
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            height="24"
+            width="24"
+            {...props}
+        >
+            <path d="M8 9c-1.628 0-3 1.372-3 3s1.372 3 3 3 3-1.372 3-3-1.372-3-3-3z" />
+            <path d="M16 6H8c-3.3 0-5.989 2.689-6 6v.016A6.01 6.01 0 008 18h8a6.01 6.01 0 006-5.994V12c-.009-3.309-2.699-6-6-6zm0 10H8a4.006 4.006 0 01-4-3.99C4.004 9.799 5.798 8 8 8h8c2.202 0 3.996 1.799 4 4.006A4.007 4.007 0 0116 16zm4-3.984l.443-.004.557.004h-1z" />
+        </svg>
+    );
+}
+
+export function ToggleRightIcon(props) {
+    return (
+        <svg
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            height="24"
+            width="24"
+            {...props}
+        >
+            <path d="M16 9c-1.628 0-3 1.372-3 3s1.372 3 3 3 3-1.372 3-3-1.372-3-3-3z" />
+            <path d="M16 6H8c-3.296 0-5.982 2.682-6 5.986v.042A6.01 6.01 0 008 18h8c3.309 0 6-2.691 6-6s-2.691-6-6-6zm0 10H8a4.006 4.006 0 01-4-3.99C4.004 9.799 5.798 8 8 8h8c2.206 0 4 1.794 4 4s-1.794 4-4 4z" />
+        </svg>
+    );
+}
---
+++ src/Assets/Icons/DocManageIcons/index.js
@@ -0,0 +1,120 @@
+export function SearchIcon(props) {
+    return (
+        <svg
+            viewBox="0 0 1024 1024"
+            fill="currentColor"
+            height="24"
+            width="24"
+            {...props}
+        >
+            <path d="M909.6 854.5L649.9 594.8C690.2 542.7 712 479 712 412c0-80.2-31.3-155.4-87.9-212.1-56.6-56.7-132-87.9-212.1-87.9s-155.5 31.3-212.1 87.9C143.2 256.5 112 331.8 112 412c0 80.1 31.3 155.5 87.9 212.1C256.5 680.8 331.8 712 412 712c67 0 130.6-21.8 182.7-62l259.7 259.6a8.2 8.2 0 0011.6 0l43.6-43.5a8.2 8.2 0 000-11.6zM570.4 570.4C528 612.7 471.8 636 412 636s-116-23.3-158.4-65.6C211.3 528 188 471.8 188 412s23.3-116.1 65.6-158.4C296 211.3 352.2 188 412 188s116.1 23.2 158.4 65.6S636 352.2 636 412s-23.3 116.1-65.6 158.4z" />
+        </svg>
+    );
+}
+
+export function DownloadIcon(props) {
+    return (
+        <svg
+            fill="none"
+            stroke="currentColor"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            viewBox="0 0 24 24"
+            height="24"
+            width="24"
+            {...props}
+        >
+            <path d="M21 15v4a2 2 0 01-2 2H5a2 2 0 01-2-2v-4M7 10l5 5 5-5M12 15V3" />
+        </svg>
+    );
+}
+
+export function UploadIcon(props) {
+    return (
+        <svg
+            fill="none"
+            stroke="currentColor"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            viewBox="0 0 24 24"
+            height="24"
+            width="24"
+            {...props}
+        >
+            <path d="M21 15v4a2 2 0 01-2 2H5a2 2 0 01-2-2v-4M17 8l-5-5-5 5M12 3v12" />
+        </svg>
+    );
+}
+
+export function CreateIcon(props) {
+    return (
+        <svg
+            fill="none"
+            stroke="currentColor"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            viewBox="0 0 24 24"
+            height="24"
+            width="24"
+            {...props}
+        >
+            <path d="M17 3a2.85 2.83 0 114 4L7.5 20.5 2 22l1.5-5.5zM15 5l4 4" />
+        </svg>
+    );
+}
+
+export function CopyIcon(props) {
+    return (
+        <svg
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            height="24"
+            width="24"
+            {...props}
+        >
+            <path d="M19 21H8V7h11m0-2H8a2 2 0 00-2 2v14a2 2 0 002 2h11a2 2 0 002-2V7a2 2 0 00-2-2m-3-4H4a2 2 0 00-2 2v14h2V3h12V1z" />
+        </svg>
+    );
+}
+
+export function DeleteForeverIcon(props) {
+    return (
+        <svg
+            fill="currentColor"
+            viewBox="0 0 16 16"
+            height="24"
+            width="24"
+            {...props}
+        >
+            <path d="M5.5 5.5A.5.5 0 016 6v6a.5.5 0 01-1 0V6a.5.5 0 01.5-.5zm2.5 0a.5.5 0 01.5.5v6a.5.5 0 01-1 0V6a.5.5 0 01.5-.5zm3 .5a.5.5 0 00-1 0v6a.5.5 0 001 0V6z" />
+            <path
+                fillRule="evenodd"
+                d="M14.5 3a1 1 0 01-1 1H13v9a2 2 0 01-2 2H5a2 2 0 01-2-2V4h-.5a1 1 0 01-1-1V2a1 1 0 011-1H6a1 1 0 011-1h2a1 1 0 011 1h3.5a1 1 0 011 1v1zM4.118 4L4 4.059V13a1 1 0 001 1h6a1 1 0 001-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"
+            />
+        </svg>
+    );
+}
+
+export function PublishIcon(props) {
+    return (
+        <svg
+            fill="currentColor"
+            viewBox="0 0 16 16"
+            height="24"
+            width="24"
+            {...props}
+        >
+            <path
+                fillRule="evenodd"
+                d="M3.5 10a.5.5 0 01-.5-.5v-8a.5.5 0 01.5-.5h9a.5.5 0 01.5.5v8a.5.5 0 01-.5.5h-2a.5.5 0 000 1h2A1.5 1.5 0 0014 9.5v-8A1.5 1.5 0 0012.5 0h-9A1.5 1.5 0 002 1.5v8A1.5 1.5 0 003.5 11h2a.5.5 0 000-1h-2z"
+            />
+            <path
+                fillRule="evenodd"
+                d="M7.646 4.146a.5.5 0 01.708 0l3 3a.5.5 0 01-.708.708L8.5 5.707V14.5a.5.5 0 01-1 0V5.707L5.354 7.854a.5.5 0 11-.708-.708l3-3z"
+            />
+        </svg>
+    );
+}

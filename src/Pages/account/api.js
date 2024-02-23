c0c26c1d53233caf62f0f61c3d093e0cdc985182
mtphuc253 <mtp25032003@gmail.com>
2024-02-23 15:50:47 +0700

    add jwt, add user role, stored loginStatus in session instead local

--- src/Pages/account/api.js
+++ src/Pages/account/api.js
@@ -1,24 +1,14 @@
-// // api.js
-// import axios from "axios";
-
-// const API_BASE_URL = "https://659e636d47ae28b0bd35b522.mockapi.io/cosmetic_shop";
-
-// const api = axios.create({
-//   baseURL: API_BASE_URL,
-// });
-
-// api.js
 import axios from "axios";
 
 const API_BASE_URL =
   "https://659e636d47ae28b0bd35b522.mockapi.io/cosmetic_shop";
 
-export const loginUser = async (username, password) => {
+export const loginUser = async (email, password) => {
   try {
     const response = await axios.get(API_BASE_URL);
 
     const user = response.data.find(
-      (user) => user.userName === username && user.password === password
+      (user) => user.email === email && user.password === password
     );
 
     return user;

342382c6d4174b80726d2277025c5cf6d708588e
khoanguyen2905 <khoanguyen2905@gmail.com>
2024-02-24 20:47:37 +0700

    Adding UI for Syllabus List and Mock data

---
+++ src/Pages/SyllabusList/CalendarPopover.js
@@ -0,0 +1,61 @@
+import React, { useState } from 'react';
+import { Button, DatePicker, Space, Popover } from 'antd';
+import { CalendarOutlined } from '@ant-design/icons';
+
+const CalendarPopover = () => {
+  const [selectedDateRange, setSelectedDateRange] = useState(null);
+  const [isPopoverVisible, setPopoverVisible] = useState(false);
+
+  const handleDateChange = (dates) => {
+    setSelectedDateRange(dates);
+  };
+
+  const clearDate = () => {
+    setSelectedDateRange(null);
+  };
+
+  const formatDateString = (date) => {
+    if (!date || !date[0] || !date[1]) return 'Create date';
+
+    const inputDate = new Date(date[0]);
+    const inputDate2 = new Date(date[1]);
+
+    const formattedDate = `${inputDate.getDate()}/${
+      inputDate.getMonth() + 1
+    }/${inputDate.getFullYear()}`;
+
+    const formattedDate2 = `${inputDate2.getDate()}/${
+      inputDate2.getMonth() + 1
+    }/${inputDate2.getFullYear()}`;
+
+    const result = `${formattedDate} - ${formattedDate2}`;
+
+    return result;
+  };
+
+  return (
+    <Popover
+      visible={isPopoverVisible}
+      onVisibleChange={(visible) => setPopoverVisible(visible)}
+      content={
+        <Space direction='vertical' style={{ width: '300px' }}>
+          <DatePicker.RangePicker
+            value={selectedDateRange}
+            onChange={handleDateChange}
+            format='DD/MM/YYYY'
+          />
+          <Button onClick={clearDate} size='small' danger>
+            Clear
+          </Button>
+        </Space>
+      }
+      trigger='click'
+    >
+      <Button shape='round' icon={<CalendarOutlined />} style={{ width: '240px' }}>
+        {formatDateString(selectedDateRange)}
+      </Button>
+    </Popover>
+  );
+};
+
+export default CalendarPopover;
---
+++ src/Pages/SyllabusList/Status.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Button, ConfigProvider } from 'antd';
+import { CloseCircleOutlined } from '@ant-design/icons';
+
+export const Status = ({ data }) => {
+  let color = '#2D3748';
+  let icon = false;
+  if (data === 'active') {
+  } else if (data === 'Inactive') {
+    color = '#B9B9B9';
+  } else if (data === 'draft') {
+    color = '#285D9A';
+  } else {
+    icon = true;
+  }
+  return (
+    <Button style={{ backgroundColor: `${color}` }} type='primary'>
+      {icon && <CloseCircleOutlined />}
+      {data}
+    </Button>
+  );
+};

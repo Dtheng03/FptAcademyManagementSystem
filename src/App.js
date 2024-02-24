d48ba99b01c1c2ddbae258393d0bb5fc7322a3a0
khoanguyen2905 <khoanguyen2905@gmail.com>
2024-02-24 20:54:03 +0700

    fix conflict

--- src/App.js
+++ src/App.js
@@ -34,62 +34,61 @@ function App() {
   }, [isLoggedIn, navigate, location.pathname]);
 
   const handleLogin = (user, token) => {
-    const handleLogin = (user, token) => {
-      setLoggedIn(true);
-      sessionStorage.setItem('isLoggedIn', JSON.stringify(true));
-      sessionStorage.setItem('userRole', user.userRole);
+    setLoggedIn(true);
+    sessionStorage.setItem('isLoggedIn', JSON.stringify(true));
+    sessionStorage.setItem('userRole', user.userRole);
 
-      // const decodedToken = jwtDecode(token);
+    // const decodedToken = jwtDecode(token);
 
-      // // Kỉm tra coi thời hạn token còn bao nhiêu (theo giây)
-      //   const currentTime = Date.now() / 1000;
-      //   if (decodedToken.exp < currentTime) {
-      //     // Token hết đát
-      //     handleLogout();
-      //     return;
-      //   }
-      // }
+    // // Kỉm tra coi thời hạn token còn bao nhiêu (theo giây)
+    //   const currentTime = Date.now() / 1000;
+    //   if (decodedToken.exp < currentTime) {
+    //     // Token hết đát
+    //     handleLogout();
+    //     return;
+    //   }
+    // }
 
-      navigate('/home');
-    };
-
-    const handleLogout = () => {
-      setLoggedIn(false);
-      sessionStorage.removeItem('isLoggedIn');
-      sessionStorage.removeItem('userRole');
-      navigate('/login');
-    };
-
-    return (
-      <div className='App'>
-        <Layout>
-          {isLoggedIn && location.pathname !== '/login' && <Header onLogout={handleLogout} />}
-          <Layout>
-            {isLoggedIn && location.pathname !== '/login' && <Sidebar />}
-            <Routes>
-              <Route path='/tranning-program-list' element={<TranningListPage />} />
-              <Route path='/user-list' element={<UserListPage />} />
-              {isLoggedIn ? (
-                <>
-                  {/* ROUTE CODE TRONG ĐÂY NHA MẤY NÍ */}
-
-                  <Route path='/home' element={<HomePage />} />
-                  <Route path='/class-list' element={<ClassListPage />} />
-                  <Route path='/user-list' element={<UserListPage />} />
-                  <Route path='/user-permission' element={<UserPermissionPage />} />
-                  <Route path='/user-permission' element={<UserPermissionPage />} />
-                  <Route path='/create-syllabus' element={<CreateSyllabusPage />} />
-                  <Route path='/materials' element={<LearningMaterials />} />
-                </>
-              ) : (
-                <Route path='/login' element={<Login onLogin={handleLogin} />} />
-              )}
-            </Routes>
-          </Layout>
-          {isLoggedIn && location.pathname !== '/login' && <Footer />}
-        </Layout>
-      </div>
-    );
+    navigate('/home');
   };
+
+  const handleLogout = () => {
+    setLoggedIn(false);
+    sessionStorage.removeItem('isLoggedIn');
+    sessionStorage.removeItem('userRole');
+    navigate('/login');
+  };
+
+  return (
+    <div className='App'>
+      <Layout>
+        {isLoggedIn && location.pathname !== '/login' && <Header onLogout={handleLogout} />}
+        <Layout>
+          {isLoggedIn && location.pathname !== '/login' && <Sidebar />}
+          <Routes>
+            <Route path='/tranning-program-list' element={<TranningListPage />} />
+            <Route path='/user-list' element={<UserListPage />} />
+            {isLoggedIn ? (
+              <>
+                {/* ROUTE CODE TRONG ĐÂY NHA MẤY NÍ */}
+
+                <Route path='/home' element={<HomePage />} />
+                <Route path='/class-list' element={<ClassListPage />} />
+                <Route path='/user-list' element={<UserListPage />} />
+                <Route path='/user-permission' element={<UserPermissionPage />} />
+                <Route path='/user-permission' element={<UserPermissionPage />} />
+                <Route path='/create-syllabus' element={<CreateSyllabusPage />} />
+                <Route path='/materials' element={<LearningMaterials />} />
+              </>
+            ) : (
+              <Route path='/login' element={<Login onLogin={handleLogin} />} />
+            )}
+          </Routes>
+        </Layout>
+        {isLoggedIn && location.pathname !== '/login' && <Footer />}
+      </Layout>
+    </div>
+  );
 }
+
 export default App;

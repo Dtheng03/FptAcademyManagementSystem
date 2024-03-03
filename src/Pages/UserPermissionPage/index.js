1d9dbb01e035f57ebb5ae5e73f3b1eed929b5047
Nguyen Duc Thang <aloducthang@gmail.com>
2024-03-03 13:01:04 +0700

    Thang: fix ui User permission

--- src/Pages/UserPermissionPage/index.js
+++ src/Pages/UserPermissionPage/index.js
@@ -42,6 +42,7 @@ function UserPermissionPage() {
     const [newPermission1, setNewPermission1] = useState({});
     const [newPermission2, setNewPermission2] = useState({});
     const [newPermission3, setNewPermission3] = useState({});
+    const [isLoading, setIsLoading] = useState(false);
 
     const handleChange = (e, item) => {
         if (item.roleName === "Super Admin") {
@@ -67,12 +68,14 @@ function UserPermissionPage() {
 
     const handleSaveUpdate = () => {
         async function update() {
+            setIsLoading(true);
             var response1, response2, response3;
             try {
                 response1 = await axios.put(`http://fams-group1-net03.ptbiology.com/api/userpermission/update-user-permission`, { ...newPermission1, userManagement: "Full access" });
                 response2 = await axios.put(`http://fams-group1-net03.ptbiology.com/api/userpermission/update-user-permission`, { ...newPermission2, userManagement: "Create" });
                 response3 = await axios.put(`http://fams-group1-net03.ptbiology.com/api/userpermission/update-user-permission`, { ...newPermission3, userManagement: "View" });
                 if (response1.statusText === "OK" && response2.statusText === "OK" && response3.statusText === "OK") {
+                    setIsLoading(false);
                     notification.success({
                         message: "Update permission successfully",
                     })
@@ -82,11 +85,15 @@ function UserPermissionPage() {
                     }, 2000)
                 }
             } catch (error) {
-                console.error(error);
                 notification.error({
                     message: "Update permission failed",
-                    description: "Please try again"
+                    description: "Something wrong! Please try again later."
                 })
+                setIsLoading(false);
+                setTimeout(() => {
+                    setUpdate(false);
+                    setIsDomChange(!isDomChange);
+                }, 2000)
             }
         }
         update();
@@ -96,118 +103,129 @@ function UserPermissionPage() {
         axios.get("http://fams-group1-net03.ptbiology.com/api/userpermission/view-user-permission")
             .then((response) => {
                 setData(response.data.data);
+                setNewPermission1(response.data.data[0]);
+                setNewPermission2(response.data.data[1]);
+                setNewPermission3(response.data.data[2]);
             })
             .catch((error) => { console.log(error); })
     }, [isDomChange]);
 
     return (
         <div className={cx("container")}>
-            <h4 className={cx("header")}>User Permission</h4>
+            {!isLoading ?
+                <>
+                    <h4 className={cx("header")}>User Permission</h4>
 
-            {/* phần actions */}
-            {roleName === "Super Admin" && < div className={cx("action")}>
-                {!update && <Button title={"Update Permission"} onClick={() => setUpdate(true)} />}
-            </div>}
+                    {/* phần actions */}
+                    {roleName === "Super Admin" && < div className={cx("action")}>
+                        {!update && <Button title={"Update Permission"} onClick={() => setUpdate(true)} />}
+                    </div>}
 
-            {/* phần hiển thị thông tin */}
-            <table className={cx("table")}>
-                <thead className={cx("thead")}>
-                    <tr className={cx("tr")}>
-                        <th className={cx("th")}>Role name</th>
-                        <th className={cx("th")}>Syllabus</th>
-                        <th className={cx("th")}>Training Program</th>
-                        <th className={cx("th")}>Class</th>
-                        <th className={cx("th")}>Learning material</th>
-                        <th className={cx("th")}>User</th>
-                    </tr>
-                </thead>
-                {!update ?
-                    <tbody className={cx("tbody")}>
-                        {data.map((item, index) => (
-                            <tr key={index} className={cx("tr")}>
-                                <td className={cx("td", "roleName")}>{item.roleName}</td>
-                                <td className={cx("td")}><Permission permission={item.syllabus} /></td>
-                                <td className={cx("td")}><Permission permission={item.trainingProgram} /></td>
-                                <td className={cx("td")}><Permission permission={item.class} /></td>
-                                <td className={cx("td")}><Permission permission={item.learningMaterial} /></td>
-                                <td className={cx("td")}><Permission permission={item.userManagement} /></td>
+                    {/* phần hiển thị thông tin */}
+                    <table className={cx("table")}>
+                        <thead className={cx("thead")}>
+                            <tr className={cx("tr")}>
+                                <th className={cx("th")}>Role name</th>
+                                <th className={cx("th")}>Syllabus</th>
+                                <th className={cx("th")}>Training Program</th>
+                                <th className={cx("th")}>Class</th>
+                                <th className={cx("th")}>Learning material</th>
+                                <th className={cx("th")}>User</th>
                             </tr>
-                        ))}
-                    </tbody>
-                    :
-                    <tbody className={cx("tbody")}>
-                        {data.map((item, index) => (
-                            <tr key={index} className={cx("tr")}>
-                                <td className={cx("td")}>{item.roleName}</td>
-                                <td className={cx("td")}>
-                                    <select
-                                        name="syllabus"
-                                        className={cx("select")}
-                                        onChange={(e) => handleChange(e, item)}
-                                    >
-                                        <option>Permission</option>
-                                        <option value={"Access denied"}>Access denied</option>
-                                        <option value={"View"}>View</option>
-                                        <option value={"Modify"}>Modify</option>
-                                        <option value={"Create"}>Create</option>
-                                        <option value={"Full access"}>Full access</option>
-                                    </select>
-                                </td>
-                                <td className={cx("td")}>
-                                    <select
-                                        name="trainingProgram"
-                                        className={cx("select")}
-                                        onChange={(e) => handleChange(e, item)}
-                                    >
-                                        <option>Permission</option>
-                                        <option value={"Access denied"}>Access denied</option>
-                                        <option value={"View"}>View</option>
-                                        <option value={"Modify"}>Modify</option>
-                                        <option value={"Create"}>Create</option>
-                                        <option value={"Full access"}>Full access</option>
-                                    </select>
-                                </td>
-                                <td className={cx("td")}>
-                                    <select
-                                        name="class"
-                                        className={cx("select")}
-                                        onChange={(e) => handleChange(e, item)}
-                                    >
-                                        <option>Permission</option>
-                                        <option value={"Access denied"}>Access denied</option>
-                                        <option value={"View"}>View</option>
-                                        <option value={"Modify"}>Modify</option>
-                                        <option value={"Create"}>Create</option>
-                                        <option value={"Full access"}>Full access</option>
-                                    </select>
-                                </td>
-                                <td className={cx("td")}>
-                                    <select
-                                        name="learningMaterial"
-                                        className={cx("select")}
-                                        onChange={(e) => handleChange(e, item)}
-                                    >
-                                        <option>Permission</option>
-                                        <option value={"Access denied"}>Access denied</option>
-                                        <option value={"View"}>View</option>
-                                        <option value={"Modify"}>Modify</option>
-                                        <option value={"Create"}>Create</option>
-                                        <option value={"Full access"}>Full access</option>
-                                    </select>
-                                </td>
-                                <td className={cx("td")}><Permission permission={item.userManagement} /></td>
-                            </tr>
-                        ))}
-                    </tbody>
-                }
-            </table>
+                        </thead>
+                        {!update ?
+                            <tbody className={cx("tbody")}>
+                                {data.map((item, index) => (
+                                    <tr key={index} className={cx("tr")}>
+                                        <td className={cx("td", "roleName")}>{item.roleName}</td>
+                                        <td className={cx("td")}><Permission permission={item.syllabus} /></td>
+                                        <td className={cx("td")}><Permission permission={item.trainingProgram} /></td>
+                                        <td className={cx("td")}><Permission permission={item.class} /></td>
+                                        <td className={cx("td")}><Permission permission={item.learningMaterial} /></td>
+                                        <td className={cx("td")}><Permission permission={item.userManagement} /></td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                            :
+                            <tbody className={cx("tbody")}>
+                                {data.map((item, index) => (
+                                    <tr key={index} className={cx("tr")}>
+                                        <td className={cx("td")}>{item.roleName}</td>
+                                        <td className={cx("td")}>
+                                            <select
+                                                name="syllabus"
+                                                className={cx("select")}
+                                                defaultValue={item.syllabus}
+                                                onChange={(e) => handleChange(e, item)}
+                                            >
+                                                <option value={"Access denied"}>Access denied</option>
+                                                <option value={"View"}>View</option>
+                                                <option value={"Modify"}>Modify</option>
+                                                <option value={"Create"}>Create</option>
+                                                <option value={"Full access"}>Full access</option>
+                                            </select>
+                                        </td>
+                                        <td className={cx("td")}>
+                                            <select
+                                                name="trainingProgram"
+                                                className={cx("select")}
+                                                defaultValue={item.trainingProgram}
+                                                onChange={(e) => handleChange(e, item)}
+                                            >
+                                                <option value={"Access denied"}>Access denied</option>
+                                                <option value={"View"}>View</option>
+                                                <option value={"Modify"}>Modify</option>
+                                                <option value={"Create"}>Create</option>
+                                                <option value={"Full access"}>Full access</option>
+                                            </select>
+                                        </td>
+                                        <td className={cx("td")}>
+                                            <select
+                                                name="class"
+                                                className={cx("select")}
+                                                defaultValue={item.class}
+                                                onChange={(e) => handleChange(e, item)}
+                                            >
+                                                <option value={"Access denied"}>Access denied</option>
+                                                <option value={"View"}>View</option>
+                                                <option value={"Modify"}>Modify</option>
+                                                <option value={"Create"}>Create</option>
+                                                <option value={"Full access"}>Full access</option>
+                                            </select>
+                                        </td>
+                                        <td className={cx("td")}>
+                                            <select
+                                                name="learningMaterial"
+                                                className={cx("select")}
+                                                defaultValue={item.learningMaterial}
+                                                onChange={(e) => handleChange(e, item)}
+                                            >
+                                                <option value={"Access denied"}>Access denied</option>
+                                                <option value={"View"}>View</option>
+                                                <option value={"Modify"}>Modify</option>
+                                                <option value={"Create"}>Create</option>
+                                                <option value={"Full access"}>Full access</option>
+                                            </select>
+                                        </td>
+                                        <td className={cx("td")}><Permission permission={item.userManagement} /></td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        }
+                    </table>
 
-            {
-                update &&
-                <div className={cx("action")}>
-                    <Button title={"Cancle"} type="underline" onClick={() => setUpdate(false)} />
-                    <Button title={"Save"} onClick={handleSaveUpdate} />
-                </div>
+                    {
+                        update &&
+                        <div className={cx("action")}>
+                            <Button title={"Cancle"} type="underline" onClick={() => setUpdate(false)} />
+                            <Button title={"Save"} onClick={handleSaveUpdate} />
+                        </div>
+                    }
+                </>
+                :
+                (<div className={cx("modal")}>
+                    <div className={cx("loading")}></div>
+                </div>)
             }
         </div >
     );

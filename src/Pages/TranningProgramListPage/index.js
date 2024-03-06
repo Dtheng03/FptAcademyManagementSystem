014be58a716465b02f45313f5dfcfdb0608e93be
An Le <anlnse171475@fpt.edu.vn>
2024-03-06 16:36:26 +0700

    Update search

--- src/Pages/TranningProgramListPage/index.js
+++ src/Pages/TranningProgramListPage/index.js
@@ -7,65 +7,31 @@ import {
   UploadIcon,
 } from "../../Components/Common/Icons/DocManageIcons";
 import Button from "../../Components/Common/Button";
-
+import { Link } from "react-router-dom";
 import {
   AddIcon,
+  CancleIcon,
   FilterListIcon,
   SortIcon,
 } from "../../Components/Common/Icons/ActionIcons";
-import { Pagination, Tag } from "antd";
+import { Pagination, Tag, Spin } from "antd";
 import Table from "./Table";
-
+import { LoadingOutlined } from "@ant-design/icons";
+import { useDispatch } from "react-redux";
+import { setProgramList } from "../../Redux/Reducer/ProgramTranningSlice";
+import ProgramList from "./ProgramList";
+import Research from "./ProgramResearch";
 const cx = classNames.bind(styles);
 export default function TranningProgramListPage() {
   const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
 
-  const [currentPage, setCurrentPage] = useState(1);
-  const [itemsPerPage, setItemsPerPage] = useState(10);
-  const startIndex = (currentPage - 1) * itemsPerPage;
-  const endIndex = startIndex + itemsPerPage;
-  const currentPageData = data.slice(startIndex, endIndex);
-
-  const [sortedInfo, setSortedInfo] = useState({});
+  const dispatch = useDispatch();
 
   const [searchValue, setSearchValue] = useState("");
-  const [filters, setFilters] = useState([]);
   const [search, setSearch] = useState([]);
 
-  const [result, setResult] = useState([]);
-
-  const [showAddModal, setShowAddModal] = useState(false);
-  const [showEditModal, setShowEditModal] = useState(false);
   const [isDomChange, setIsDomChange] = useState(false);
-
-  const handleSort = (columnKey) => {
-    let sortOrder =
-      sortedInfo.columnKey === columnKey && sortedInfo.order === "ascend"
-        ? "descend"
-        : "ascend";
-    setSortedInfo({ columnKey, order: sortOrder });
-  };
-
-  const sortedData = data.sort((a, b) => {
-    const columnKey = sortedInfo.columnKey;
-    const order = sortedInfo.order === "ascend" ? 1 : -1;
-
-    if (columnKey === "name") {
-      return a.name.localeCompare(b.name) * order;
-    } else if (columnKey === "id") {
-      return (a.id - b.id) * order;
-    } else if (columnKey === "createOn") {
-      return a.createOn.localeCompare(b.createOn) * order;
-    } else if (columnKey === "createBy") {
-      return a.createBy.localeCompare(b.createBy) * order;
-    } else if (columnKey === "duration") {
-      return (a.duration - b.duration) * order;
-    } else if (columnKey === "status") {
-      return a.status.localeCompare(b.status) * order;
-    }
-
-    return 0;
-  });
 
   const handleInputSearch = (e) => {
     const value = e.target.value;
@@ -73,23 +39,34 @@ export default function TranningProgramListPage() {
 
     if (value.trim() === "") {
       setSearch([]);
-    } else {
+    } else if (value !== "") {
       const searchResults = data.filter((result) =>
         result.name.toLowerCase().includes(value.toLowerCase())
       );
-      setSearch(searchResults);
+      if (searchResults.length > 0) {
+        setSearch(searchResults);
+      } else {
+        setSearch([]);
+      }
     }
   };
 
   useEffect(() => {
     async function getProgram() {
       try {
+        setLoading(true);
         const response = await axios.get(
           "https://65411666f0b8287df1fdc4fa.mockapi.io/program"
         );
+        // const response = await axios.get(
+        //   "http://fams-group1-net03.ptbiology.com//api/trainingprogram/view-training-program-list"
+        // );
         setData(response.data);
+        dispatch(setProgramList(response.data));
       } catch (error) {
         console.error(error);
+      } finally {
+        setLoading(false);
       }
     }
     getProgram();
@@ -97,129 +74,86 @@ export default function TranningProgramListPage() {
 
   return (
     <div className={cx("container")}>
-      <h3 className={cx("header")}>Tranning program</h3>
-      <div className={cx("action")}>
-        <div className={cx("search")}>
-          <div className={cx("search-input")}>
-            <SearchIcon />
-            <input
-              className={cx("input-contain")}
-              type="text"
-              value={searchValue}
-              onChange={handleInputSearch}
-              placeholder="Search by..."
-            />
+      <Spin
+        indicator={<LoadingOutlined style={{ color: "#2D3748" }} />}
+        spinning={loading}
+        size="default"
+        style={{
+          position: "fixed",
+          top: "50%",
+          left: "50%",
+          transform: "translate(-50%,-50%",
+        }}
+      >
+        <h3 className={cx("header")}>Tranning program</h3>
+        <div className={cx("action")}>
+          <div className={cx("search")}>
+            <div className={cx("search-input")}>
+              <SearchIcon />
+              <input
+                className={cx("input-contain")}
+                type="text"
+                value={searchValue}
+                onChange={handleInputSearch}
+                placeholder="Search by..."
+
+              />
+              {searchValue !== "" &&
+              <button className={cx("clear-search-btn")}
+              onClick={() =>{
+                setSearch([]);
+                setSearchValue("");
+              }
+
+              }>
+              <CancleIcon/>
+              </button>
+              }
+            </div>
+            <Button title="Filter" firstIcon={<FilterListIcon />} />
           </div>
-          <Button title="Filter" firstIcon={<FilterListIcon />} />
+          <div className={cx("btn-grp")}>
+            <button className={cx("import-btn")}>
+              <UploadIcon style={{ marginRight: "13px" }} />
+              Import
+            </button>
+            <Link to="/create-program">
+              <Button title="Add New" firstIcon={<AddIcon />} />
+            </Link>
+          </div>
         </div>
-        <div className={cx("btn-grp")}>
-          <button className={cx("import-btn")}>
-          <UploadIcon style={{ marginRight: '13px' }} /> 
-            Import
-          </button>
-          <Button title="Add New" firstIcon={<AddIcon />} />
+
+        <div className={cx("search-result")}>
+          <Tag className={cx("result")} color="#474747" closable>
+            cc
+          </Tag>
         </div>
-      </div>
 
-      <div className={cx("search-result")}>
-        <Tag className={cx("result")} color="#474747" closable>
-          cc
-        </Tag>
-      </div>
-
-      <table className={cx("table")}>
-        <thead className={cx("thead")}>
-          <tr className={cx("tr")}>
-            <th className={cx("th", "id")}>
-              <button className={cx("title")} onClick={() => handleSort("id")}>
-                ID <SortIcon />
-              </button>
-            </th>
-
-            <th className={cx("th", "id")}>
-              <button
-                className={cx("title")}
-                onClick={() => handleSort("name")}
-              >
-                Program name <SortIcon />
-              </button>
-            </th>
-            <th className={cx("th", "id")}>
-              <button
-                className={cx("title")}
-                onClick={() => handleSort("createOn")}
-              >
-                Create on <SortIcon />
-              </button>
-            </th>
-            <th className={cx("th", "id")}>
-              <button
-                className={cx("title")}
-                onClick={() => handleSort("createBy")}
-              >
-                Create by <SortIcon />
-              </button>
-            </th>
-            <th className={cx("th", "id")}>
-              <button
-                className={cx("title")}
-                onClick={() => handleSort("duration")}
-              >
-                Duration <SortIcon />
-              </button>
-            </th>
-            <th className={cx("th", "id")}>
-              <button
-                className={cx("title")}
-                onClick={() => handleSort("status")}
-              >
-                Status <SortIcon />
-              </button>
-            </th>
-            <th className={cx("th")}></th>
-          </tr>
-        </thead>
-        {search.length > 0 ? (
-          <tbody className={cx("tbody")}>
-            {search.map((item) => (
-              <Table
-                key={item.id}
-                item={item}
-                domChange={() => setIsDomChange(true)}
-                domChangeSuccess={() => setIsDomChange(false)}
-              />
-            ))}
-          </tbody>
-        ) : (
-          <tbody className={cx("tbody")}>
-            {currentPageData.map((item) => (
-              <Table
-                key={item.id}
-                item={item}
-                domChange={() => setIsDomChange(true)}
-                domChangeSuccess={() => setIsDomChange(false)}
-              />
-            ))}
-          </tbody>
-        )}
-      </table>
-
-      {(search.length > 0 || data.length > 0) && (
-        <div className={cx("pagination")}>
-          <Pagination
-            onChange={(page, pageSize) => {
-              setItemsPerPage(pageSize);
-              setCurrentPage(page);
+        {searchValue === "" ? (
+          <ProgramList
+            domChange={() => {
+              setIsDomChange(true);
             }}
-            showSizeChanger
-            onShowSizeChange={(pageSize) => {
-              setItemsPerPage(pageSize);
+            domChangeSuccess={() => setIsDomChange(false)}
+            reload={() => {
+              setSearch([]);
+              setSearchValue("");
             }}
-            current={currentPage}
-            total={data.length}
           />
-        </div>
-      )}
+        ) : (
+          <Research
+            search={search}
+            domChange={() => {
+              setIsDomChange(true);
+            }}
+            domChangeSuccess={() => setIsDomChange(false)}
+            reload={() => {
+              setSearch([]);
+              setSearchValue("");
+            }}
+          />
+        )}
+      </Spin>
     </div>
   );
 }

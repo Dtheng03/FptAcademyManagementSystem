41ab6e7a627a566953eb93326af8d0db95b88b5c
khoanguyen2905 <khoanguyen2905@fpt.edu.vn>
2024-03-20 21:46:22 +0700

    adding loading spinning middle

--- src/Pages/SyllabusList/MenuOption.js
+++ src/Pages/SyllabusList/MenuOption.js
@@ -155,8 +155,11 @@ const MenuOption = ({ item, apiData, setApiData, status }) => {
       <Spin
         style={{
           position: 'fixed',
+          top: '50%',
+          left: '50%',
+          transform: 'translate(-50%, -50%)',
           zIndex: '9999',
-          backgroundColor: 'rgba(0, 0, 0, 0.1)',
+          backgroundColor: 'rgba(0, 0, 0, 0.2)',
         }}
         spinning={loading}
       >
